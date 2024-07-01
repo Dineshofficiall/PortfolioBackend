2d8909a393dffb102611798f608f6e060f96df44
Dinesh Kumar <dineshsaravananofficial@gmail.com>
2024-07-01 21:55:48 +0530

    first commit

---
+++ src/main/java/com/portfolio/PortfolioProject/PortfolioProjectApplication.java
@@ -0,0 +1,12 @@
+package com.portfolio.PortfolioProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioProjectApplication.class, args);
+	}
+}
